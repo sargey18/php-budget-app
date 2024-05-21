3147b3c2c4385171ceb71e3df738f3a3f9feebbb
Ryan Sargeson <35943901+sargey18@users.noreply.github.com>
2024-05-21 23:16:21 +0700

    working on the routing

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,10 +2,15 @@
 
 declare(strict_types=1);
 
-include __DIR__ . '/../Framework/App.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
 use Framework\App;
+use APP\Controllers\HomeController;
+
 
 $app = new App();
+
+$app->get('/', [HomeController::class, 'home']);
+
 
 return $app;
